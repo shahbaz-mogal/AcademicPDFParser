8b154f6ca5536bf5ca7cc60cb8997a5d9fb6f8c5
Shahbaz Mogal <shahbazmogal@outlook.com>
2023-12-24 13:48:35 -0800

    Added citation

--- predict.py
+++ predict.py
@@ -1,3 +1,12 @@
+"""
+Copyright (c) Meta Platforms, Inc. and affiliates.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+
+Being reused by Shahbaz Mogal (shahbazmogal@outlook.com)
+for educational purposes.
+"""
 import argparse
 from pathlib import Path
 import logging
